48d0899dadea0101495f17c4510fcce2a12bc5e4
Lucas Sá <lucasdelimasa@gmail.com>
2022-04-14 19:14:40 -0300

    Adjust Login Function

--- Projeto PROF_FILE/SCRIPT/script.js
+++ Projeto PROF_FILE/SCRIPT/script.js
@@ -1,7 +1,21 @@
+const login = 'admin';
+const password = 'admin';
+
 function showLoginWindow () { 
     if ( document.querySelector(".popUpWindow").style.visibility == 'hidden'){
         document.querySelector(".popUpWindow").style.visibility = "visible";
     } else {
         document.querySelector(".popUpWindow").style.visibility = "hidden";    
+    }
+}
+
+function checkPassword () {
+    while (login === userLogin) {
+        if (password === userPassword) {
+            console.log('Login e senha correto!')
+        } else {
+                console.log('Login e senha errado!')
+            }
+        
     }
 }
