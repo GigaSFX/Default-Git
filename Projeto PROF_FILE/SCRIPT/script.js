b1a32468fc0d1012e84c9ca13bac80d63dbf6ad6
Lucas Sá <lucasdelimasa@gmail.com>
2022-04-17 20:29:43 -0300

    adjust frame, link to forgot, size of frame and countdown

--- Projeto PROF_FILE/SCRIPT/script.js
+++ Projeto PROF_FILE/SCRIPT/script.js
@@ -11,9 +11,13 @@ function showLoginWindow () {
 }
 
 function checkPassword () {
-    if (login !== document.querySelector('#userLogin').value || password !== document.querySelector('#userPassword').value && count < 3) {
+    if (login !== document.querySelector('#userLogin').value || password !== document.querySelector('#userPassword').value) {
         alert('Login or Password Worg! Please try again or Try create a new Account.');
         count++;
+        if (count >= 4) {
+            alert('Wait 30 min, util try another Login! Or Click in "Forget your Password?" to change your Password.');
+            windows.location.replace("index.html")
+        }
     } else {    
     window.location.replace("url");
     }
